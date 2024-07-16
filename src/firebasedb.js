ff14c57a0d3f257822485f4a6abf44e8c9158b5f
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-07-16 11:46:54 +0530

    firebase

---
+++ src/firebasedb.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBww7K2WsMPbpwAFb0d2cV1NgNxwPcn0M0",
+  authDomain: "todo-firebase-b2309.firebaseapp.com",
+  projectId: "todo-firebase-b2309",
+  storageBucket: "todo-firebase-b2309.appspot.com",
+  messagingSenderId: "761815034777",
+  appId: "1:761815034777:web:0dd70ce7c2de2a9c1b0108",
+  measurementId: "G-GBH65C4N0V"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
